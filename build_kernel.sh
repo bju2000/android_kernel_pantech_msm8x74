fc3674e74bdca7cd5a5f4ef2fe55994730143324
diszell2008 <listening2009@gmail.com>
2015-08-15 23:03:08 +0700

    Enable intellithermal

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
-cp defconfig .config
-make -j4
+export CROSS_COMPILE=/media/diszell2008/Build_Teo/cm-12.0/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+make cyanogenmod_a910_defconfig
+make -j16 zImage 
