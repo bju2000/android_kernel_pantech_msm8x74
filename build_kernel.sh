a0b4248f3cdf5610281c229c7ec90f3ed17f580d
chautruongthinh <chautruongthinh@tekcafe.vn>
2016-06-08 01:24:18 +0700

    Enable last_kmsg in kernel msm3.4

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export ARCH=arm
-export CROSS_COMPILE=/media/diszell2008/Build_Teo/cm-12.0/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=/home/chautruongthinh/cm13/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 make cyanogenmod_a910_defconfig
-make -j16 zImage 
+make -j4 zImage 
